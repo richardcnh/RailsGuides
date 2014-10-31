9447892da02f8822629f781db9881dcc7c44e1e4
Richard Cai <rcai@xogrp.com>
2014-10-31 12:01:39 +0800

    Complete training

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with(name: "rcai", password: "120", except: [:index, :show])
+
     def new
-        
+        @article = Article.new
     end
 
     def create
@@ -10,12 +12,39 @@ class ArticlesController < ApplicationController
         #
         @article = Article.new(article_params)
  
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render "new"
+        end
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
     end
 
     private
